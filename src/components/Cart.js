f03fc13c5305e78cc656899371337af078bc43d0
jasonLapina <lapina.jason@gmail.com>
2023-11-27 09:20:45 +0800

    cart popover style changes

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -13,20 +13,27 @@ function Cart() {
   return (
     <Box mt='16px' bgColor='#f6f6f7' maxW='1440px' px='16px' mx='auto'>
       <Box display='flex' maxW='var(--maxW)' justifyContent='end' mx='auto'>
-        <Popover>
-          <PopoverTrigger>
-            <Box as='button' fontSize='12px' py='6px' fontWeight='semibold'>
-              My Cart ( 4 )
-            </Box>
-          </PopoverTrigger>
-          <PopoverContent>
-            <PopoverArrow />
-            <PopoverCloseButton />
-            <PopoverHeader>Confirmation!</PopoverHeader>
-            <PopoverBody>
-              Are you sure you want to have that milkshake?
-            </PopoverBody>
-          </PopoverContent>
+        <Popover offset={[-120, 0]}>
+          {({ isOpen }) => (
+            <>
+              <PopoverTrigger>
+                <Box
+                  as='button'
+                  fontSize='12px'
+                  py='6px'
+                  fontWeight='semibold'
+                  color={isOpen ? "#222" : "#888"}
+                >
+                  My Cart ( 4 )
+                </Box>
+              </PopoverTrigger>
+              <PopoverContent borderRadius='none'>
+                <PopoverBody>
+                  Are you sure you want to have that milkshake?
+                </PopoverBody>
+              </PopoverContent>
+            </>
+          )}
         </Popover>
       </Box>
     </Box>
